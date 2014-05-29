b009b69c5e579765187f06bce313cdce87117511
Chih-Yung Liang <stu89424@gmail.com>
2014-05-30 03:43:42 +0800

    Logout when not login

--- js/abPost.js
+++ js/abPost.js
@@ -1,13 +1,16 @@
 function abPost(target, data, successCallback, otherCallbacks) {
 	$.post(target, data, function(data, status) {
 		if(status != "success") {
-			/* TODO */
+			alert("Connection error! Please contact site manager");
 		} else if(data.status == 'success') {
 			successCallback(data.data);
 		} else if(otherCallbacks && data.status in otherCallbacks) {
 			otherCallbacks[data.status]();
+		} else if(data.status == 'not_login') {
+			alert("Please re-login.");
+			location.reload();
 		} else {
-			/* TODO */
+			alert("It seems that something is not going right.");
 		}
 	}, 'json');
 }
